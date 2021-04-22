d1c7164eab4cc5de10ddacddce42dc1d24b0105a
Gogor Putra Hafi Puja Gusti <80830527+GogorPutra@users.noreply.github.com>
2021-04-22 22:08:10 +0700

    Add files via upload

---
+++ LatihanAlgoritma/PercobaanNIM.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LatihanAlgoritma;
+
+import java.util.Scanner;
+
+/** 
+ *
+ * @author USER
+ */
+public class PercobaanNIM {
+  
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nama: ");
+        String nama = input.nextLine();
+        System.out.print("NIM : ");
+        String nim = input.nextLine();
+
+        //String nama = "Gogor Putra Hafi Puja Gusti";
+        //String nim = "12050112140";
+        NIM objNim = new NIM();
+        objNim.setNim(nim);
+        System.out.println("Jenjang Perndidikan = "+objNim.getJenjang());
+        System.out.println("Tahun Masuk = 20"+objNim.getTahun());
+        System.out.println("Fakultas = "+objNim.getFakultas());
+        System.out.println("Jurusan = "+objNim.getJurusan());
+        System.out.println("Jenis kelamin = "+objNim.getJenisKelamin());
+        System.out.println("Nomor Urut Mahasiswa = "+objNim.getNomorurut());
+    }
+}
